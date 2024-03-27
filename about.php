e020d1cbba24f55bcd29a6ae3a35a95e238d20aa
johnN34 <yohanesnancu95@gmail.com>
2024-03-27 12:11:49 +0700

    website travel

---
+++ about.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- header section starts -->
+
+<section class="header">
+
+<a href="home.php" class="logo">travel</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg.png) no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!-- about section starts -->
+
+<section class="about">
+
+    <div class="images">
+        <img src="images/about.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+           Aut delectus aliquam dolores nam eligendi fugit sit nostrum,
+           aperiam tenetur quibusdam.
+        </p>
+        <p>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+           Aut delectus aliquam dolores nam eligendi fugit sit nostrum,
+           aperiam tenetur quibusdam.
+        </p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>top destinations</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable price</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 guide service</span>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts -->
+
+<section class="reviews">
+
+    <h1 class="heading-title"> client reviews </h1>
+
+    <div class="swiper reviews-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit beatae illum rem modi, cupiditate doloribus dicta officia. Ab modi maxime, ipsam perferendis obcaecati, aspernatur quis vitae error fugiat atque aliquam!
+                </p>
+                <h3>john doe</h3>
+                <span>traveler</span>
+                <img src="images/pic-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, molestias! Modi unde nihil eligendi facilis eos, quidem voluptatem quis. Totam culpa corrupti iusto impedit veritatis, ipsum architecto nemo rerum neque aut optio, cumque laudantium, illo aliquid id! Quidem, praesentium neque!
+                </p>
+                <h3>john doe</h3>
+                <span>traveler</span>
+                <img src="images/pic-2.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Corrupti, neque?
+                </p>
+                <h3>john doe</h3>
+                <span>traveler</span>
+                <img src="images/pic-3.jpg" alt="">
+            </div>
+
+            <!-- <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque obcaecati laudantium officiis tenetur quaerat ipsa.
+                </p>
+                <h3>john doe</h3>
+                <span>traveler</span>
+                <img src="images/pic-4.jpg" alt="">
+            </div> -->
+
+            <!-- <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab est voluptate unde, eum fuga sit, placeat deserunt dolor consequatur dolore repellat quibusdam laborum facilis a corrupti voluptas, sed ratione harum perspiciatis. Asperiores fuga quam amet!
+                </p>
+                <h3>john doe</h3>
+                <span>traveler</span>
+                <img src="images/pic-5.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non officiis consequatur porro quia quam, vitae alias delectus cum illum odit.
+                </p>
+                <h3>john doe</h3>
+                <span>traveler</span>
+                <img src="images/pic-6.jpg" alt="">
+            </div> -->
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+<!-- footer section stars --> 
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask question</a>
+            <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>      
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +6287 854710031</a>
+            <a href="#"><i class="fas fa-phone"></i> +6287 854710031 </a>
+            <a href="#"><i class="fas fa-envelope"></i> john6342@gmail.com </a>
+            <a href="#"><i class="fas fa-map"></i> indonesia </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+
+        </div>
+
+    </div>
+    <div class="credit"> created by <span>mr. John</span> | all right reserved! </div>
+ 
+</section>
+
+<!-- footer section ends --> 
+
+
+<!-- swiper js link -->
+<script src="https://unpkg.com/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
